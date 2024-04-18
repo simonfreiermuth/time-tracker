824ab867ae79eec6506ee11544f1ee96935ba833
Simon Freiermuth <simon.freiermuth1@students.fhnw.ch>
2024-04-18 23:16:08 +0200

    AI generated icon

--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,7 +1,8 @@
-import { Moon, Sun, Timer } from "@phosphor-icons/react";
-import { ActionButton, Header, SegmentedField, Text, Tooltip, fr, usePrismaneColor, usePrismaneTheme, useThemeModeValue } from "@prismane/core";
+import { Moon, Sun } from "@phosphor-icons/react";
+import { ActionButton, Flex, Header, Image, SegmentedField, Text, Tooltip, fr, usePrismaneTheme, useThemeModeValue } from "@prismane/core";
 import { useAppStore } from "../data/stores";
 import { set } from "idb-keyval";
+import logo from "../../public/icon.png";
 
 interface HeaderBarProps {
     /** Show the data switch (assumes data store was initialized) */
@@ -11,7 +12,6 @@ interface HeaderBarProps {
 export default function HeaderBar({ dataSwitch }: HeaderBarProps) {
     const toggleThemeModeStore = useAppStore(s => s.toggleThemeMode);
     const { toggleThemeMode, theme } = usePrismaneTheme();
-    const { getColor } = usePrismaneColor();
 
     const dataSource = useAppStore().dataSource;
     const setDataSource = useAppStore().setDataSource;
@@ -24,10 +24,13 @@ export default function HeaderBar({ dataSwitch }: HeaderBarProps) {
     return (
         <Header
             bg={useThemeModeValue(["primary", 200, 0.5], ["primary", 900, 0.3])}
-            p={fr(4)} gap={fr(2)}
+            p={fr(2)} gap={fr(4)}
             justify="end" align="center"
         >
-            <Text as="h1" mr="auto" >Time tracker <Timer size={32} weight="fill" color={getColor("primary")} /> </Text>
+            <Flex direction="row" gap={fr(4)} align="center" mr="auto">
+                <Image src={logo} h={fr(16)} fit="contain" />
+                <Text as="h1" mr="auto">Time tracker</Text>
+            </Flex>
             {dataSwitch && <Tooltip
                 position="bottom-end" size="md" color="primary"
                 label="Store data in a local file or browsers localStorage" >
